74f73b484c81c23cddb812fc9dcb819e3c8fbaa3
Sandro Santos <sandro.dev@gmail.com>
2020-01-01 02:51:55 -0300

    format price and new rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,34 +19,9 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
-    "no-params-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
-  },
-};
-module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "import/prefer-default-export": "off"
   },
 };
