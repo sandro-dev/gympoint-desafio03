e7f27d5a7bc9e0f458fdd0f836881c989a2b9961
Sandro Santos <sandro.dev@gmail.com>
2020-01-01 01:52:41 -0300

    mailtrap config and Mail class

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '774f6a5bd0165a',
+    pass: '803809041bf4ab',
+  },
+  default: {
+    from: 'Equipe Gympoint <noreply@gympoint.com>',
+  },
+};
