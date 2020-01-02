f0db31048e2ee330c78e4f5a72dc6270c74e1dcf
Sandro Santos <sandro.dev@gmail.com>
2020-01-02 18:29:37 -0300

    no-console rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
-    "import/prefer-default-export": "off"
+    "import/prefer-default-export": "off",
+    "no-console": "off",
   },
 };
